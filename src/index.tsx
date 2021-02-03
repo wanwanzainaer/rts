ef4f1bbca494238aa9395a557f2cf8469b984598
simon <wanwanzainaer@gmail.com>
2021-02-02 20:43:30 -0800

    Add ref and event exampler

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 // import { GuestList } from './state/GuestList';
-import { UserSearch } from './state/UserSearch';
-
+// import { UserSearch } from './state/UserSearch';
+// import { EventComponent } from './events/EventComponent';
+import { UserSearch } from './refs/UserSearch';
 const App = () => {
   return (
     <div>
